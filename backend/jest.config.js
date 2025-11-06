6ee12023599982154a5a09d6f555b8345c17e81e
Makatendeka Chikumbu <91256825+makac1896@users.noreply.github.com>
2025-11-06 14:34:27 -0800

    Fix coverage reports

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -15,13 +15,17 @@ module.exports = {
   testTimeout: 30000,
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/controllers/friends.controller.ts',
-    'src/controllers/location.controller.ts',
-    'src/controllers/badge.controller.ts',
-    'src/routes/friends.routes.ts',
-    'src/routes/location.routes.ts',
-    'src/routes/badge.routes.ts',
+    'src/**/*.{js,ts}',
+    '!src/**/*.d.ts',
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 15,
+      functions: 30,
+      lines: 30,
+      statements: 30
+    }
+  },
 };
